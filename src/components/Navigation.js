8c0f5b74e0d16d746f3f38a0bebdadda32a56585
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-11-01 14:00:18 -0700

    add navigation bar component

---
+++ src/components/Navigation.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { NavLink, useLocation } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+
+const NavigationBar = () => {
+  const location = useLocation();
+
+  const homeLinkStyle = (pathname) => {
+    if (pathname === '/' || pathname === '/home') {
+      return `${NavigationStyles.linkStyles} ${NavigationStyles.navLinkBorder}  ${NavigationStyles.active}`;
+    }
+    return `${NavigationStyles.linkStyles} ${NavigationStyles.navLinkBorder} `;
+  };
+
+  const portfolioLinkStyle = (pathname) => {
+    if (pathname === '/portfolio') {
+      return `${NavigationStyles.linkStyles} ${NavigationStyles.navLinkBorder} ${NavigationStyles.active}`;
+    }
+    return `${NavigationStyles.linkStyles} ${NavigationStyles.navLinkBorder}`;
+  };
+
+  const aboutLinkStyle = (pathname) => {
+    if(pathname === '/about') {
+      return `${NavigationStyles.linkStyles} ${NavigationStyles.navLinkBorder} ${NavigationStyles.navLinkBorder} ${NavigationStyles.active}`;
+    }
+    return `${NavigationStyles.linkStyles} ${NavigationStyles.navLinkBorder}`;
+  };
+
+  const contactLinkStyle = (pathname) => {
+    if(pathname === '/about') {
+      return `${NavigationStyles.linkStyles} ${NavigationStyles.marginLeft} ${NavigationStyles.navLinkBorder} ${NavigationStyles.active}`;
+    }
+    return `${NavigationStyles.linkStyles} ${NavigationStyles.marginLeft}`;
+  };
+
+  return (
+    <nav className={NavigationStyles.navbar}>
+      <div className={NavigationStyles.logo}>
+        <h1>Emmanuel Simasiku</h1>
+      </div>
+      <div className={NavigationStyles.linksCont}>
+        <NavLink key={uuidv4()}  className={homeLinkStyle(location.pathname)} to="/">
+          Home
+        </NavLink>
+        <NavLink key={uuidv4()} className={portfolioLinkStyle(location.pathname)} to="/portfolio">
+          Portfolio
+        </NavLink>
+        <NavLink key={uuidv4()} className={aboutLinkStyle(location.pathname)} to="/about">
+          About
+        </NavLink>
+        <NavLink key={uuidv4()} className={contactLinkStyle(location.pathname)} to="/contac-us">
+          Contact 
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationBar
