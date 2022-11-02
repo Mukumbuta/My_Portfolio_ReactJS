e38fa964c7fb188b34ff90b3d21dcda18236f2c8
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-11-02 12:43:23 -0700

    fix linter errors

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import NavigationStyles from './NavigationStyles.module.css';
 
 const NavigationBar = () => {
@@ -37,34 +37,32 @@ const NavigationBar = () => {
   };
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
         <div>
-          <a class="navbar-brand" href="#">Emmanuel Simasiku</a>
+          <a className="navbar-brand" href="About">Emmanuel Simasiku</a>
         </div>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <NavLink key={uuidv4()} className={`${homeLinkStyle(location.pathname)} nav-link`} to="/">
-            Home
-          </NavLink>
-          <NavLink key={uuidv4()} className={`${portfolioLinkStyle(location.pathname)} nav-link`} to="/portfolio">
-            Portfolio
-          </NavLink>
-          <NavLink key={uuidv4()} className={`${aboutLinkStyle(location.pathname)} nav-link`} to="/about">
-            About
-          </NavLink>
-          <NavLink key={uuidv4()} className={`${contactLinkStyle(location.pathname)} nav-link`} to="/contact-us">
-            Contact
-          </NavLink>
+          <div className="navbar-nav">
+            <NavLink key={uuidv4()} className={`${homeLinkStyle(location.pathname)} nav-link`} to="/">
+              Home
+            </NavLink>
+            <NavLink key={uuidv4()} className={`${aboutLinkStyle(location.pathname)} nav-link`} to="/about">
+              About
+            </NavLink>
+            <NavLink key={uuidv4()} className={`${portfolioLinkStyle(location.pathname)} nav-link`} to="/portfolio">
+              Portfolio
+            </NavLink>
+            <NavLink key={uuidv4()} className={`${contactLinkStyle(location.pathname)} nav-link`} to="/contact-us">
+              Contact
+            </NavLink>
+          </div>
         </div>
-       </div>
       </div>
     </nav>
-      
-
   );
 };
 
